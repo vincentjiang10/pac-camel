39bd7cfffd899f7c70cfa130879086e40696f295
vj68@cornell.edu <vincentjiang2003@gmail.com>
2022-10-15 19:46:14 -0400

    Update files

---
+++ src/coin.mli
@@ -0,0 +1,7 @@
+(**Representation of a coin*)
+type t
+
+val big_coin_image : t -> string
+val small_coin_image : t -> string
+val big_coin_val : t -> int
+val small_coin_val : t -> int
--- src/pacmap.mli
+++ src/pacmap.mli
@@ -7,3 +7,9 @@ type t
 
 val valid_move : t -> float * float -> bool
 val start_pos : t -> float * float
+
+(* gen_map: generates random map (may take in a seed arg for a seeded random
+   map) *)
+(* draw_map: called by main to draw original map *)
+(* cover_cell: called by main when camel crosses a cell with coin -> draw white
+   square on top of coin *)
