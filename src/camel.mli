5ec61758102bffe592428923ab75e43bf13a8968
yg298 <yg298@cornell.edu>
2022-10-15 19:17:28 -0400

    Implement camel.mli and coin.ml

---
+++ src/camel.mli
@@ -0,0 +1,12 @@
+(** Representation of camel in game.
+
+    This module represents the data stored in a camel, including it's x and y
+    position. *)
+
+(** The abstract type of values representing a camel*)
+type t
+
+val get_x : t -> float
+val get_y : t -> float
+val move : t -> Pacmap.t -> float * float -> t
+val init : Pacmap.t -> string -> t
---
+++ src/coin.ml
@@ -0,0 +1,13 @@
+(* coin logic *)
+
+type t = {
+  bSrc : string;
+  sSrc : string;
+  bVal : int;
+  sVal : int;
+}
+
+let big_coin_image t = t.bSrc
+let small_coin_image t = t.sSrc
+let big_coin_val t = t.bVal
+let small_coin_val t = t.sVal
