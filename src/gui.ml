fdf041dc97aeae35a8ed7f972f21cf68c08cebcf
wy120303 <dianawu1203@gmail.com>
2022-10-16 19:15:05 -0400

    Updated our gui.ml
    -updated image for the camel

--- src/gui.ml
+++ src/gui.ml
@@ -18,6 +18,10 @@ let start_button_w = W.button "Start the Game"
 let start_title_l = Layout.resident start_title_w ~y:2
 let start_button_l = Layout.resident ~x:100 ~y:25 ~h:10 start_button_w
 
+let camel_image =
+  let src = W.image ~w:40 ~h:40 "data/images/camel-cartoon.png" in
+  L.flat ~name:"image" [ L.resident src ]
+
 (* TODO @GUI: add to this function, which should initialize gui widgets (be
    prepared to take in functions that should be called based on widget events)*)
 let greeting =
@@ -29,7 +33,7 @@ let greeting =
   (*action to be connected to start button*)
   let start_action _ _ _ =
     print_endline "start";
-    L.set_rooms layout []
+    L.set_rooms layout [ camel_image ]
     (*this line replace current layout with an empty list, add widgets (to be
       drawn after start) in this list e.g. map, camel, human etc. *)
   in
